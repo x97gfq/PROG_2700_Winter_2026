ca14080eccc550f7909b70445f1214a1c97ef9c8
Jamie Symonds <jamie.symonds@gmail.com>
2026-01-22 20:05:44 -0400

    updated Week 3 Activity 1

--- Week3/Activity1/script.js
+++ Week3/Activity1/script.js
@@ -1,41 +1,27 @@
 //declare variables (get references to box, score)
-let scoreEl = document.getElementById("score");
-let boxEl = document.getElementById("box");
+
 
 //set initial score to 0
-let score = 0;
+
 
 // Function declaration: increaseScore
-function increaseScore() {
-    //console.log("increaseScore called");
-    score++;
-    scoreEl.textContent = score;
-}
+
 
 // Function declaration: moveBox
-function moveBox() {
-    //console.log("moveBox called")
-    const maxX = Math.max(0, window.innerWidth-100);
-    const maxY = Math.max(0, window.innerHeight-100)
-    
-    const x = Math.random() * maxX;
-    const y = Math.random() * maxY;
 
-    boxEl.style.left = x + 'px';
-    boxEl.style.top = y + 'px';
-}
 
-function changeBoxColour() {
-    //console.log("changeBoxColour called");
-    boxEl.style.background = `hsl(${Math.floor(Math.random()*360)}, 70%, 55%)`;
-}
+// Function change box colour
+
 
 // Function expression: handleClick
 const handleClick = function () {
     //console.log("handleClick==----------------------------------")
-    increaseScore();
-    changeBoxColour();
-    moveBox(); 
+
+    //call a function to increase score
+
+    //call a function to move box
+
+    //call a function to change box colour
 };
 
 box.addEventListener('click', handleClick);
